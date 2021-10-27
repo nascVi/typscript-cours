f7dcf3d67587f098d9da478cda8bd97016ecbed1
vaiiho <vaiiho@localhost.localdomain>
2021-10-27 12:02:43 +0200

    Company class + index imports 🛋️

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,20 +1,18 @@
 import faker from 'faker'
 
-// the use of default key-word in React?
-// export const red = 'red';
-
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
-
   constructor() {
-    this.name = faker.name.firstName();     
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
-    };
+    }
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there!');
+import { User/*, red*/ } from './User';
+import  { Company } from './Company';
+
+const user = new User();
+
+console.log(user);
+
+const company = new Company();
+
+console.log(company);
