d3c4e43f9ef30cef6745ab2ef91834e2b64e5baa
nascvi <vaiiho@localhost.localdomain>
2021-12-07 09:40:33 +0100

    data updated for stats🌉

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('hey!');
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('ranking.csv');
+reader.read();
+// const matches = fs
+// .readFileSync(basketball.csv, {
+//   encoding: 'utf-8'
+// })
+// .split('\n')
+// .map(
+//   (row: string): string[] => {
+//     return row.split(',');
+//   }
+// );
+// // enum - enumeration
+// enum MatchResult {
+// HomeWin = 'H',
+// AwayWin = 'A',
+// Draw = ''
+// }
+// let BullsWins = 0;
+// for(let match of reader.data) {
+//   if(matche[1] === 'Bulls' && match[5] === MatchResult)
+// }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,2 +1,27 @@
-console.log('hey!');
+import { CsvFileReader } from './CsvFileReader';
 
+const reader = new CsvFileReader('ranking.csv');
+reader.read();
+// const matches = fs
+// .readFileSync(basketball.csv, {
+//   encoding: 'utf-8'
+// })
+// .split('\n')
+// .map(
+//   (row: string): string[] => {
+//     return row.split(',');
+//   }
+// );
+
+// // enum - enumeration
+// enum MatchResult {
+  // HomeWin = 'H',
+  // AwayWin = 'A',
+  // Draw = ''
+// }
+
+// let BullsWins = 0;
+
+// for(let match of reader.data) {
+//   if(match[5] === 'Bulls' && match[7] === MatchResult)
+// }
